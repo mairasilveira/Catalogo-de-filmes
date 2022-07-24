6e5f321abcc9c076070bfdece45bf079d1cb0196
Maira Silveira <silveirasmaira@gmail.com>
2022-07-24 19:27:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var myArray = ['Stranger things', 'The witcher', 'Forgotten', 'The boys', 'Arremessando alto'];
+
+
+var listaFilmes = ["https://conteudo.imguol.com.br/c/entretenimento/58/2022/05/12/novo-poster-da-quarta-temporada-de-stranger-things-1652368177430_v2_3x4.jpg", "https://br.web.img3.acsta.net/pictures/19/11/29/17/57/5161763.jpg", "https://woomagazine.com.br/wp-content/uploads/2018/03/Rastros-de-um-sequestro-poster.jpg", "https://pbs.twimg.com/media/FNgNKFIUYAMw8bI?format=jpg&name=900x900","https://br.web.img3.acsta.net/pictures/22/05/25/20/17/5781885.jpg", "https://media-exp1.licdn.com/dms/image/D4E12AQFIjYyS1Pq7ww/article-cover_image-shrink_720_1280/0/1653492231322?e=1660176000&v=beta&t=8wC_X8fdDETmjCIPrUR2hbSuTD70g5ANMC_U4R43pBw", "https://br.web.img3.acsta.net/pictures/19/03/19/11/47/0630842.jpg" ]
+
+for (var i = 0; i < listaFilmes.length; i++){
+  document.write("<img src=" +  listaFilmes[i] + ">")
+  
+} 
+
